bfe67bd12e2b6a51aa7cce4fbd50fcab1b7dcec2
findikbey <44061774+findikbey@users.noreply.github.com>
2020-05-18 22:55:08 +0300

    Update keys.js

--- practice-app/back-end/routes/keys.js
+++ practice-app/back-end/routes/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
-	consumer_key: 'IN69pKsnfsgXRWDyZN9ktM4me',
-	consumer_secret: 'PU8RYfSAdeATpupXrrN6xhJRePPn6p0tm8HPzdp1AhPwWFJsAR',
-	access_token_key: '360626802-UWGxOJmy5qRwoamZLxCpFFjd6D10KkuOMoyitm67',
-	access_token_secret: 'wQ62fHnY70hM4OljMAzBxOkZPLbDpQkcNheFodR97H4xD'
+	consumer_key: '',
+	consumer_secret: '',
+	access_token_key: '',
+	access_token_secret: ''
 }
 
