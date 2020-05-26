48d9f7e1274ed2cf73da599b8b202c6a3055772d
İsmet Sarı <49066101+sarismet@users.noreply.github.com>
2020-05-26 18:05:29 +0300

    Update filter_user_tweets.js

--- practice-app/back-end/routes/filter_user_tweets.js
+++ practice-app/back-end/routes/filter_user_tweets.js
@@ -1,37 +1,56 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET 'count' amount of tweets about 'keyword' from user 'username'  */
-router.get('/', function(req, res, next) {
+/* GET twitter comments. */
+router.get('/', function (req, res, next) {
+  // import twitter
+  var Twitter = require('twitter');
+  //get the all access keys
+  var key = require('./keys.js');
+  //create new twitter object
+  var client = new Twitter(key);
+  //To specify searching parameters
+  var params = {
+    q: req.query.product_name, //get the product name
+    count: 100,
+    lang: 'tr' //see which language the program search
+  };
 
-    //initiliaze twitter object with token from key.js
-    var Twitter = require('twitter');
-    var key = require('./keys.js');
-    var client = new Twitter(key);
+  //defining dictiornary to store tweets
+  var dict = {};
 
-    //paramater to be used in get method
-    var params = {
-        q: 'from:' + req.query.user + ' @' + req.query.mentioned,
-        count: req.query.count
+      sorted_array = [];
+  //gets the tweets here
+  client.get('/search/tweets/', params, function (error, data, response) {
+    if (!error) { //if there is no error
+      var tweets = data.statuses; //take tweets status
+      for (var i = 0; i < tweets.length; i++) {
+        dict[tweets[i].text] = tweets[i].favorite_count; //defining pairs for dictionary
+      }
+	    try{
+      //get the keys and mapp them with its regarding values
+      items = Object.keys(dict).map(function (key) {
+        return [key, dict[key]];
+      });
+      //sorted them one by one
+      items.sort(function (first, second) {
+        return second[1] - first[1];
+      });
+      //create array for sorted verision
+      //fill this array with first 10 tweets
+      for (var i = 0; i < 10; i++) {
+        sorted_array.push(items[i][0]);
+      }
+	    }
+	    catch(err){
+console.log("there is an error");
+	    }
+      //create the json file
+      var myJsonString = JSON.stringify(sorted_array);
+      //and show them on screen
+      res.json(myJsonString);
+
     }
-
-    //use Twitter API to get tweets defined above
-    client.get('search/tweets', params,  function(error, data, response){
-        if(error) throw error;
-        else {
-            //initialize empty list to collect data
-            var tweetList = [];
-
-            //interested in statuses part of data
-            var tweets = data.statuses;
-
-            //add text parts of tweets to the list
-            for (var i = 0; i<tweets.length; i++)
-                tweetList.push(tweets[i].text)
-
-            //send JSON response
-            res.json(JSON.stringify(tweetList));
-        }
     }); 
 });
 
