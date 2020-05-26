2c799b54d14594f40b6ad94445a66ba8e378d9f2
Muhammed <muhammedhalas@gmail.com>
2020-05-26 19:43:53 +0300

    Revert Errors
    This reverts commit

--- practice-app/back-end/routes/database.js
+++ practice-app/back-end/routes/database.js
@@ -96,7 +96,7 @@ router.post('/addproduct', function(req, res){
         console.log(err);
       } else {
         // Redirect to the updated product list
-        res.json(result);
+        res.send(result);
       }
       // Close the database
       client.close();
