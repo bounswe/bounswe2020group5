1a58bb4ad422c91c9e2f3995a61707630707434e
Emre <34emredemir42@gmail.com>
2020-05-27 01:25:34 +0200

    unit tests added

--- practice-app/back-end/test/test-database.js
+++ practice-app/back-end/test/test-database.js
@@ -11,11 +11,44 @@ chai.use(chaiHttp);
 
 describe("db api", () => {
     // test root
+    describe("GET /database", () => {
+        it("Root should respond", (done) => {
+            chai.request(server)
+                .get("/database")
+                .end((err, response) => {
+                    response.should.have.status(200);
+                done();
+                });
+        });
+    });
+
 
     // test list
+    describe("GET /database/thelist", () => {
+        it("It should get all products", (done) => {
+            chai.request(server)
+                .get("/database/thelist")
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
+                done();
+                });
+        });
+    });
 
     // test new product
-
+    describe("GET /database/newproduct", () => {
+        it("It should render an html form", (done) => {
+            chai.request(server)
+                .get("/database/newproduct")
+                .end((err, response) => {
+                    response.should.have.status(200);
+                    response.should.be.html;
+                done();
+                });
+        });
+    });
+    
     // test add product
     describe("GET /database/addproduct", () => {
         it("It should add a new product to products db", (done) => {
