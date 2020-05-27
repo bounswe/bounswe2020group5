d9b7dd5807cf5e3b7584f75ce44431f1dde5fa98
Yaşar Selçuk Çalışkan <yscaliskan@gmail.com>
2020-05-27 16:32:56 +0300

    Update exchangerates_test.js

--- practice-app/back-end/test/exchangerates_test.js
+++ practice-app/back-end/test/exchangerates_test.js
@@ -1,8 +1,6 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../app.js');
-let should = chai.should();
-let expect = chai.expect;
 
 chai.should();
 chai.use(chaiHttp);
