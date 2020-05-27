c4c2d4d603a26f978e708f510a9ac4051173a9e8
Yaşar Selçuk Çalışkan <yscaliskan@gmail.com>
2020-05-27 16:32:10 +0300

    Add files via upload

---
+++ practice-app/back-end/test/exchangerates_test.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app.js');
+let should = chai.should();
+let expect = chai.expect;
+
+chai.should();
+chai.use(chaiHttp);
+
+describe("Unit testing exchangerates", () => {    
+    describe("GET /exchangerates", () => {
+        it("Should return exchange rates based on the Turkish lira", (done) => {
+            chai.request(server)
+                .get("/exchangerates")
+                .end((err, response) => {
+
+                    //test response properties
+                    response.should.have.status(200);
+                    response.should.have.property('ok').eq(true);
+                    response.should.have.property('error').eq(false);
+                    response.should.have.property('badRequest').eq(false);
+                    response.should.have.property('unauthorized').eq(false);
+                    response.should.have.property('notFound').eq(false);
+                    response.body.should.have.property('rates');
+                    response.body.should.have.property('base');
+                    response.body.should.have.property('date');
+                    chai.expect(response.body.status.code).to.equal(200);
+                done();
+                });
+        });
+    });
+
+});
