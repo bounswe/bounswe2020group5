5847fae3c1b0b6eda0810861f603747bd1ec1254
Yusuf Yüksel <1996yusufyuksel@gmail.com>
2020-11-11 01:27:59 +0300

    api layer and necessary folders are created

---
+++ app/backend/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views.users import UserViewSet 
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='users')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
