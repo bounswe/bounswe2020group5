9d1d2a0e4782967f93d567dcc81e2685974d8b6c
emrehoser <emrehoser@gmail.com>
2020-11-17 14:44:37 +0300

    mobile\ some version changes and view component changes are done.

--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/core/BaseActivity.kt
+++ app/mobile/BuPazar/app/src/main/java/com/example/bupazar/core/BaseActivity.kt
@@ -1,12 +1,26 @@
 package com.example.bupazar.core
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import android.view.inputmethod.InputMethodManager
 import com.example.bupazar.R
 
-class BaseActivity : AppCompatActivity() {
+
+open class BaseActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if(currentFocus != null) {
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+        }
+        return super.dispatchTouchEvent(ev)
     }
 }
--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/activity/login/LoginActivity.kt
+++ app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/activity/login/LoginActivity.kt
@@ -1,12 +1,29 @@
 package com.example.bupazar.page.activity.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.FirebaseApp
 import com.example.bupazar.R
+import com.example.bupazar.`interface`.ActivityChangeListener
+import com.example.bupazar.core.BaseActivity
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity(), ActivityChangeListener {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        FirebaseApp.initializeApp(applicationContext)
     }
+
+    override fun changeActivity(activity: AppCompatActivity) {
+        val intent = Intent(this, activity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
+
 }
