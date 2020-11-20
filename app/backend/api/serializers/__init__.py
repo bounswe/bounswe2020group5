dcea401a40532096aca0803d1a4cfa2be93cbe96
Yusuf Yüksel <1996yusufyuksel@gmail.com>
2020-11-20 15:49:16 +0300

    backend/ LoginSerializer and AuthSerializer are added

---
+++ app/backend/api/serializers/__init__.py
@@ -0,0 +1,4 @@
+from .users import UserSerializer
+from .users import LoginSerializer
+from .users import AuthUserSerializer
+from .users import EmptySerializer
--- app/backend/api/serializers/users.py
+++ app/backend/api/serializers/users.py
@@ -1,9 +1,31 @@
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+from ..models.users import User
+from rest_framework.authtoken.models import Token
 from rest_framework import serializers
 
-#TODO: User Serializer will be populated.
+usr = get_user_model()
+
 #User Serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email']
+        fields = '__all__'
+
+class LoginSerializer(serializers.Serializer):
+    email = serializers.CharField(max_length=200, required=True)
+    password = serializers.CharField(max_length=128, required=True, write_only=True)
+
+class AuthUserSerializer(serializers.ModelSerializer):
+    auth_token = serializers.SerializerMethodField()
+
+    class Meta:
+        model = usr
+        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'is_customer', 'is_vendor', 'is_active', 'is_staff', 'address', 'auth_token']
+        read_only_fields = ['id', 'is_active', 'is_staff']
+
+    def get_auth_token(self, obj):
+        token, dummy = Token.objects.get_or_create(user=obj)
+        return token.key        
+    
+class EmptySerializer(serializers.Serializer):
+    pass
