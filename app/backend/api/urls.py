033bb9405f6d13b3976b9c2f2690cd059420c8f5
Yusuf Yüksel <1996yusufyuksel@gmail.com>
2020-11-20 16:09:39 +0300

    backend/ urls are updated

--- app/backend/api/urls.py
+++ app/backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views.users import UserViewSet
+from .views import *
 from django.views.generic import TemplateView
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
+router.register(r'auth', AuthViewSet, basename='auth')
 
 urlpatterns = [
     path('', include(router.urls)),
--- app/backend/bupazar/urls.py
+++ app/backend/bupazar/urls.py
@@ -19,7 +19,7 @@ from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('api/', include('api.urls')),
     path('openapi/', get_schema_view(
         title="Bupazar",
         description="API Documentation for Developers"
