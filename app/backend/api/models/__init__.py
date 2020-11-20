cd326d0dc0f57a0df07034f0c0921586ba7dc56e
Yusuf Yüksel <1996yusufyuksel@gmail.com>
2020-11-20 15:20:19 +0300

    backend/ database models are added

---
+++ app/backend/api/models/__init__.py
@@ -0,0 +1,3 @@
+from .users import User
+from .users import Customer
+from .users import Vendor
--- app/backend/bupazar/settings.py
+++ app/backend/bupazar/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 ]
 
+AUTH_USER_MODEL = 'api.User'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
