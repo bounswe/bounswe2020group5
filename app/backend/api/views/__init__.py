f2334bb38f557d249c71ff7b8598410d9182e48f
Volkan Bulca <volkan.bulca98@gmail.com>
2020-12-13 18:02:31 +0300

    backend/ Comment views are added and urls are updated

--- app/backend/api/views/__init__.py
+++ app/backend/api/views/__init__.py
@@ -4,4 +4,4 @@ from .products import ProductViewSet
 from .products import ProductOptViewSet
 from .products import ProductListViewSet
 from .products import ProductListOptViewSet
-
+from .products import CommentViewSet
