a898bc525b44ebe55ffd911406d1f0ed6d767b04
yscaliskan <yscaliskan@gmail.com>
2020-12-18 22:46:45 +0300

    done implementing the navigation bar

--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/model/Product.kt
+++ app/mobile/BuPazar/app/src/main/java/com/example/bupazar/model/Product.kt
@@ -7,7 +7,10 @@ data class Product (val name: String, val description: String, val productNo: In
             , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMcHcX5QR0TaJ5hYUvuFno4lfDUksgI-gPMA&amp;usqp=CAU"
             , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMpQ6N7pKsyd4spN9lOmD_ND7yqvD1-I1RNw&amp;usqp=CAU"
             , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLpR0nohEuF5_KKNkvpZdGo6gI_gm9rPzdqw&amp;usqp=CAU"
-            , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2OEdAAT2mRuuVS51rmNY8_08SfL5ZQ30DYQ&amp;usqp=CAU")
+            , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2OEdAAT2mRuuVS51rmNY8_08SfL5ZQ30DYQ&amp;usqp=CAU"
+            , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2OEdAAT2mRuuVS51rmNY8_08SfL5ZQ30DYQ&amp;usqp=CAU"
+            , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2OEdAAT2mRuuVS51rmNY8_08SfL5ZQ30DYQ&amp;usqp=CAU"
+            , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLpR0nohEuF5_KKNkvpZdGo6gI_gm9rPzdqw&amp;usqp=CAU")
 //    val imageUrl = "https://picsum.photos/150?random=$productNo"
     val imageUrl = urlData.get(productNo)
 }
