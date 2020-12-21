213a72399b2ce4ccaf3b56ad399baf74187de7cf
yscaliskan <yscaliskan@gmail.com>
2020-12-21 22:07:43 +0300

    gridview to show products is added.

--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/model/Product.kt
+++ app/mobile/BuPazar/app/src/main/java/com/example/bupazar/model/Product.kt
@@ -2,7 +2,7 @@ package com.example.bupazar.model
 
 
 
-data class Product (val name: String, val description: String, val productNo: Int){
+data class Product (val name: String, val price: String, val productNo: Int){
     private val urlData = arrayOf("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ2VZX0397KBcVYrj7K4vbhZHj6zPkMQPqpJw&amp;usqp=CAU"
             , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMcHcX5QR0TaJ5hYUvuFno4lfDUksgI-gPMA&amp;usqp=CAU"
             , "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMpQ6N7pKsyd4spN9lOmD_ND7yqvD1-I1RNw&amp;usqp=CAU"
