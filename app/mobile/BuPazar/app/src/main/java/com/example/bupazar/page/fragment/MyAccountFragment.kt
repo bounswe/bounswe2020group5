7c986f0703a81e100c4d76afc4ef46b5fb947677
emrehoser <emrehoser@gmail.com>
2020-12-21 22:19:23 +0300

    mobile-logout button is activated.
    user could tap logout button and go to login page.

--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/fragment/MyAccountFragment.kt
+++ app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/fragment/MyAccountFragment.kt
@@ -1,5 +1,6 @@
 package com.example.bupazar.page.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,6 +9,9 @@ import android.view.ViewGroup
 import android.widget.TextView
 import com.example.bupazar.R
 import com.example.bupazar.model.LoginResponse
+import com.example.bupazar.page.activity.login.LoginActivity
+import com.example.bupazar.page.activity.login.RegisterActivity
+import kotlinx.android.synthetic.main.fragment_my_account.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,6 +41,11 @@ class MyAccountFragment : Fragment() {
         userNameTextView.text = "${userData?.firstName} ${userData?.lastName}"
         mailTextView = view.findViewById(R.id.accountPageUserEmail)
         mailTextView.text = "${userData?.userEmail}"
+
+        rightArrow6.setOnClickListener(){
+            var intent=Intent(this.activity, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 
