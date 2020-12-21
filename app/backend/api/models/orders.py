650bb4ba8e17db6a9ad4ba8bf0b3f1aed958c5e4
Volkan Bulca <44063929+volkanbulca@users.noreply.github.com>
2020-12-22 00:47:48 +0300

    Update orders.py

--- app/backend/api/models/orders.py
+++ app/backend/api/models/orders.py
@@ -2,7 +2,7 @@ from django.db import models
 from .users import Customer
 
 class CreditCard(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(default='my credit card', max_length=50)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     card_owner = models.CharField(max_length=250)
     card_number = models.CharField(max_length=16)
