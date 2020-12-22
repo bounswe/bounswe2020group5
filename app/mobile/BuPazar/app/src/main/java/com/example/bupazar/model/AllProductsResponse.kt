173576145e8fcf79bfac865dcb26e0a0d85a4eab
Kayacan <kayacanvesek@gmail.com>
2020-12-22 13:15:08 +0300

    mobile/ not used, Delete AllProductsResponse

--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/model/AllProductsResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.example.bupazar.model
-
-import com.google.gson.annotations.SerializedName
-
-data class AllProductsResponse (
-    @SerializedName("products") val all_products: Array<ProductDetails>?,
-)
