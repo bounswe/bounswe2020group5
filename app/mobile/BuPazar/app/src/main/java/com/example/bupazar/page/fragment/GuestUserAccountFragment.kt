cc82750c3775ee6be45a0edfe36a1cc70387aa1a
emrehoser <emrehoser@gmail.com>
2020-12-24 20:06:25 +0300

    mobile-  pages modifief according to guest user

--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/fragment/GuestUserAccountFragment.kt
+++ app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/fragment/GuestUserAccountFragment.kt
@@ -1,58 +1,36 @@
 package com.example.bupazar.page.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.bupazar.R
+import com.example.bupazar.page.activity.login.LoginActivity
+import com.example.bupazar.page.activity.login.RegisterActivity
+import kotlinx.android.synthetic.main.fragment_guest_user_account.*
+import kotlinx.android.synthetic.main.fragment_my_account.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [GuestUserAccountFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class GuestUserAccountFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_guest_user_account, container, false)
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment GuestUserAccountFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                GuestUserAccountFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+        loginButton.setOnClickListener(){
+            var intent= Intent(this.activity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+        registerButton.setOnClickListener(){
+            var intent= Intent(this.activity, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
