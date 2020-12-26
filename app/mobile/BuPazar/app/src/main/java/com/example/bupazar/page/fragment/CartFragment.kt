dc33cf9f3ea93ffbe00d081065ec8c85dcc28872
yscaliskan <yscaliskan@gmail.com>
2020-12-26 23:30:48 +0300

    cart page adapter connection

--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/fragment/CartFragment.kt
+++ app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/fragment/CartFragment.kt
@@ -6,11 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.bupazar.R
-import com.example.bupazar.model.CartProduct
-import com.example.bupazar.model.LoginResponse
-import com.example.bupazar.model.ProductsInCart
+import com.example.bupazar.model.*
 import com.example.bupazar.service.RestApiService
+import kotlinx.android.synthetic.main.fragment_cart.*
+import kotlinx.android.synthetic.main.fragment_homepage.*
 import kotlinx.android.synthetic.main.fragment_product.*
 
 private const val ARG_AUTH_TOKEN = "authToken"
@@ -46,6 +48,13 @@ class CartFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cart, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val cartProductAdapter = this.context?.let { productsInCart?.let { it1 -> CartProductAdapter(it, cartProducts = it1) } }
+        cartProducts.adapter = cartProductAdapter
+        cartProducts.layoutManager = LinearLayoutManager(this.context)
     }
 
     companion object {
