c9c87be025df2d30afd65203b75581dab9b564f9
misraya <misraayavuz98@hotmail.com>
2020-12-28 18:41:39 +0300

    frontend/ custom tab, search lowercase handled

--- app/frontend/src/components/gridlistforsearch.js
+++ app/frontend/src/components/gridlistforsearch.js
@@ -19,12 +19,14 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: theme.palette.background.paper,
     },
     gridList: {
-        width: 1110,
+        width: 1200,
         height: "max-content",
     },
-
     icon: {
         color: 'rgba(255, 255, 255, 0.54)',
+    },
+    tile: {
+        marginBottom: "1rem",
     },
 }));
 
@@ -37,19 +39,19 @@ export const TitlebarGridList= ({tileData, categoryPage}) =>  {
         <div style={{marginTop:'2rem'}} className={classes.root}>
             <GridList cellHeight={400}  className={classes.gridList}>
                 {!categoryPage && localStorage.getItem('searchkey')!=='undefined'? (
-                    <GridListTile  cols={2} style={{ height: 'auto' }}>
+                    <GridListTile className={classes.tile} cols={2} style={{ height: 'auto' }}>
                         <h3 style={{ marginBottom:'1rem'}}>Search Result For :
                             &nbsp; {localStorage.getItem('searchkey')}</h3>
                     </GridListTile>
                 ) : null}
                 {tileData.length===0 ? (
-                    <GridListTile  cols={2} style={{ height: '55rem' }}>
+                    <GridListTile className={classes.tile} cols={2} style={{ height: '55rem' }}>
                         <h3 style={{ marginBottom:'1rem',alignText:"center"}}>There are no available products. </h3>
                     </GridListTile>
                 ):(
                     tileData.map((tile) => (
 
-                        <GridListTile key={tile.img} cols={tile.cols || 2/3} >
+                        <GridListTile className={classes.tile} key={tile.img} cols={tile.cols || 2/3} >
                             {categoryPage ? (
                                 <Link to={"/product/"+tile.id}>
                                 <img  style={{width:"21rem",height:"20rem"}} src={tile.image_url} alt={tile.name} /></Link>
@@ -57,15 +59,24 @@ export const TitlebarGridList= ({tileData, categoryPage}) =>  {
                             <Link to={{pathname: `product/${tile.id}`}}>
                                 <img  style={{width:"21rem",height:"20rem"}} src={tile.image_url} alt={tile.name} /></Link>
                           )}
-                            <GridListTileBar  style={{ backgroundColor:'rgb(211,211,211,.7)',width:"22rem",height:"10rem"}}
-                            title={<span style={{color:'black',fontSize:"1.2rem", width:"max-component"}}>{tile.name.toUpperCase() }
-                                <span style={{color: "red",fontSize:"1.2rem"}} > &nbsp; &nbsp; %{tile.discount} </span> <Divider/>  <br></br> </span>}
-                                              subtitle={<span  style={{color: "black",fontSize:"0.8rem"}}>PRICE: {tile.price}
+                            {tile.discount >0 ? (
+                                <GridListTileBar  style={{ backgroundColor:'rgb(211,211,211,.7)',width:"22rem",height:"10rem"}}
+                                                  title={<span style={{color:'black',fontSize:"1.2rem", width:"max-component"}}>{tile.name.toUpperCase() }
+                                                      <span style={{color: "red",fontSize:"1.2rem"}} > &nbsp; &nbsp; %{tile.discount} </span> <Divider/>  <br></br> </span>}
+                                                  subtitle={<span  style={{color: "black",fontSize:"0.9rem"}}>PRICE: ${tile.price} <span style={{color: "red",fontSize:"0.9rem"}} > &nbsp; ---> &nbsp;  ${(tile.price - tile.price * tile.discount / 100).toFixed(2)} </span>
+                                                      <br></br><br></br>BY: {tile.vendor}
+                                                      <br></br><br></br>
+                                <Rating name="size-small" value={tile.rating} precision={0.1}  readOnly size="small" /> </span>}
+                                />
+                                ): (
+                                <GridListTileBar  style={{ backgroundColor:'rgb(211,211,211,.7)',width:"22rem",height:"10rem"}}
+                                title={<span style={{color:'black',fontSize:"1.2rem", width:"max-component"}}>{tile.name.toUpperCase() } <Divider/>  <br></br> </span>}
+                                subtitle={<span  style={{color: "black",fontSize:"0.8rem"}}>PRICE: ${tile.price}
                                 <br></br><br></br>BY: {tile.vendor}
                                 <br></br><br></br>
                                 <Rating name="size-small" value={tile.rating} precision={0.1}  readOnly size="small" /> </span>}
-
-                        />
+                                />
+                                )}
                     </GridListTile>
                 )))}
 
