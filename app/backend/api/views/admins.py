d556b59ee9f27388f86f28910be5d552b49252ee
sarismet <sarismet2825@gmail.com>
2021-01-20 20:44:55 +0300

    backend/ deleting comment, deleting user with id and comment id and assigning admin endpoints are re-implemented to solve crashed swagger issue

--- app/backend/api/views/admins.py
+++ app/backend/api/views/admins.py
@@ -36,3 +36,71 @@ class AdminViewSet(viewsets.GenericViewSet):
         if Admin.objects.filter(username=request.user.username) :
             is_found = True
         return Response(data={"is_it_admin":is_found}, status=status.HTTP_200_OK)
+
+    #this endpoint gives a random user an admin capability.
+    @swagger_auto_schema(method='post', responses={status.HTTP_201_CREATED: SuccessSerializer, status.HTTP_400_BAD_REQUEST: ErrorSerializer})
+    @action(methods=['POST'], detail=False)
+    def assign_admin(self, request):
+        username = request.data.get("username")
+        #first we take the admin with that username and if it is not exist we return an error since the user who send the request is not an admin
+        if not Admin.objects.filter(username=request.user.username) :
+            return Response(data={'error': 'You are not authorized to assign a user admin mode'}, status=status.HTTP_400_BAD_REQUEST)
+        #then check if the user that we intend to assign an admin mode is already an admin or not
+        if Admin.objects.filter(username=username):
+            return Response(data={'error': 'the user is already admin'}, status=status.HTTP_400_BAD_REQUEST)
+        #If it is suitable to be an admin we get the user
+        try:
+            usr_to_assign = User.objects.get(username=username)
+        except:
+            return Response(data={'error': 'the user not found with that username'}, status=status.HTTP_400_BAD_REQUEST)
+        a = Admin(email=usr_to_assign.email, username=username)
+        a.save()#give him or her an admin mode
+        return Response(data={"success":"admin is assigned"}, status=status.HTTP_201_CREATED)
+
+    #this endpoint delete a comment
+    @swagger_auto_schema(method='post', responses={status.HTTP_201_CREATED: SuccessSerializer, status.HTTP_400_BAD_REQUEST: ErrorSerializer})
+    @action(methods=['POST'], detail=False)
+    def delete_comment(self, request):
+        comment_id = request.data.get("comment_id")
+        #first we take the admin with that username and if it is not exist we return an error since the user who send the request is not an admin
+        if not Admin.objects.filter(username=request.user.username):
+            return Response(data={'error': 'the user is not admin'}, status=status.HTTP_400_BAD_REQUEST)
+        is_found = Comment.objects.filter(id=comment_id)# we try to get the comment here with id.
+        if not is_found:#if it is not found we return an error.
+            return Response(data={'error': 'Comment is not found with that id'}, status=status.HTTP_400_BAD_REQUEST)
+        is_found.delete()#deleting the comment
+        return Response(data={"success":"comment is deleted"}, status=status.HTTP_201_CREATED)
+
+    #this endpoint delete a user by finding the user with a comment id
+    @swagger_auto_schema(method='post', responses={status.HTTP_201_CREATED: SuccessSerializer, status.HTTP_400_BAD_REQUEST: ErrorSerializer})
+    @action(methods=['POST'], detail=False)
+    def delete_user_by_comment_id(self, request):
+        comment_id = request.data.get("comment_id")
+        #first we take the admin with that username and if it is not exist we return an error since the user who send the request is not an admin
+        if not Admin.objects.filter(username=request.user.username):
+            return Response(data={'error': 'the user is not admin'}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            comment = Comment.objects.get(id=comment_id)#try to get the comment
+        except:
+            return Response(data={'error': 'Comment is not found with that id'}, status=status.HTTP_400_BAD_REQUEST)
+        #getting the owner of the comment
+        user_with_that_commnet_id = comment.customer
+        #deleting
+        user_with_that_commnet_id.delete()
+        return Response(data={"success":"customer user is deleted"}, status=status.HTTP_201_CREATED)
+        
+    #this endpoint delete a user with just his or her id
+    @swagger_auto_schema(method='post', responses={status.HTTP_201_CREATED: SuccessSerializer, status.HTTP_400_BAD_REQUEST: ErrorSerializer})
+    @action(methods=['POST'], detail=False)
+    def delete_user_by_username(self, request):
+        username = request.data.get("username")
+        #first we take the admin with that username and if it is not exist we return an error since the user who send the request is not an admin
+        if not Admin.objects.filter(username=request.user.username):
+            return Response(data={'error': 'the user is not admin'}, status=status.HTTP_400_BAD_REQUEST)
+        #trying to get the user
+        user = User.objects.filter(username=username)
+        if not user: #if it is not found return an errror
+            return Response(data={'error': 'the user not found with that id'}, status=status.HTTP_400_BAD_REQUEST)
+        # deleting the user
+        user.delete()
+        return Response(data={"success":"customer user is assigned"}, status=status.HTTP_201_CREATED) 
