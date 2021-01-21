9e80dd3abbf9546ac66c93465dbdfd3509baa0a4
misraya <misraayavuz98@hotmail.com>
2021-01-21 21:38:36 +0300

    frontend/ types of products vendor sells
    vendors can see the union of subcategories of their products in my products page. (part of requirement 1.1.1.3.2.)

---
+++ app/frontend/src/vendorproduct/VendorProductTypes.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Chip from "@material-ui/core/Chip";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+    },
+    chip: {
+        backgroundColor: "#0B3954",
+        fontSize: "1.3rem",
+        color: "white",
+        marginRight: "1rem",
+        marginBottom: "1rem",
+    },
+}));
+
+export const ProductTypes = ({typeslist}) =>  {
+
+    const classes = useStyles();
+    return (
+        <div>
+            <div className={classes.root}>
+                <List component="nav" className={classes.root} aria-label="mailbox folders">
+                    <Typography variant="h5" style={{display: 'inline-block', margin:"1rem"}}  gutterBottom>
+                        Types of products I sell:
+                    </Typography>
+                    {typeslist.map((type, index) => (
+                        <Chip className={classes.chip} label={type} />
+                    ))}
+                </List>
+            </div>
+        </div>
+    );
+}
+export default ProductTypes;
+
