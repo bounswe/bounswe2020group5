25c630e5e1548c480000f6b26bb178db48d2114f
Muhammed Halas <muhammedhalas@gmail.com>
2021-01-21 22:34:25 +0300

    Notification model added
    Notification model added according to group feedback

---
+++ app/backend/api/models/notifications.py
@@ -0,0 +1,26 @@
+from django.db import models
+from datetime import datetime 
+import json
+from enum import Enum 
+from .users import User
+from .orders import Order
+
+
+class NotificationType(Enum):
+    STOCK_ENDED = 1
+    STOCK_RENEWED = 2
+    ORDER_STATUS_CHANGED = 3
+    NEW_DISCOUNT = 4
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
+class Notification(models.Model):
+    text = models.TextField()
+    notificationType = models.IntegerField(choices=CustomerTypes.choices())
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
