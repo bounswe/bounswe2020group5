e1a120948318908fe92bc01414f601064d1fdfb1
Muhammed Halas <muhammedhalas@gmail.com>
2021-01-23 21:19:12 +0300

    Updated Notification endpoints
    previous my_notifications endpoint implementation did not work so I updated the viewset.
    now users notifications can be GET from notifications/my
    and al notifications can be GET from
    notifications/all

--- app/backend/api/views/notifications.py
+++ app/backend/api/views/notifications.py
@@ -1,25 +1,32 @@
 from rest_framework import viewsets
 from ..serializers import NotificationSerializer
 from ..models import Notification
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import action, api_view, permission_classes
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework import status
+from api.custom_permissions import IsAuthCustomer
 
-class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Notification.objects.all()
-    serializer_class = NotificationSerializer
+class NotificationViewSet(viewsets.GenericViewSet):
+    permission_classes = [IsAuthCustomer, AllowAny]
+    serializer_classes = {
+        'all': NotificationSerializer,
+        'my': NotificationSerializer,
+    }
+    @swagger_auto_schema(method='get', responses={status.HTTP_200_OK: NotificationSerializer(many=True)})
+    @action(methods=['GET', ], detail=False, permission_classes=[AllowAny])
+    def all(self, request):
+        notifications = Notification.objects.all()
+        content = NotificationSerializer(notifications, many=True)
+        return Response(data=content.data, status=status.HTTP_200_OK)
 
-"""
-Returns notifications of user
-"""
-@swagger_auto_schema(method='get', responses={status.HTTP_200_OK: NotificationSerializer(many=True)})
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def my_notifications(request):
-    user = request.user
-    notifications = Notification.objects.filter(user=user)
-    content = NotificationSerializer(notifications, many=True)
+    @swagger_auto_schema(method='get', responses={status.HTTP_200_OK: NotificationSerializer(many=True)})
+    @action(methods=['GET', ], detail=False, permission_classes=[IsAuthCustomer,])
+    def my(self, request):
+        user = request.user
+        notifications = Notification.objects.filter(user=user)
+        content = NotificationSerializer(notifications, many=True)
+        return Response(data=content.data, status=status.HTTP_200_OK)
 
-    return Response(data=content.data, status=status.HTTP_200_OK)
+    
