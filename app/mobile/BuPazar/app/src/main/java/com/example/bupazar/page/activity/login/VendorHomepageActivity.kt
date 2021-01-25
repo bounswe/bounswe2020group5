4bfd9a40271b324643cc27c52450f187eca7199d
yscaliskan <yscaliskan@gmail.com>
2021-01-26 02:04:45 +0300

    vendor orders page xml implementation

--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/activity/login/VendorHomepageActivity.kt
+++ app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/activity/login/VendorHomepageActivity.kt
@@ -8,11 +8,15 @@ import com.example.bupazar.R
 import com.example.bupazar.User
 import com.example.bupazar.page.activity.message.MessageMainActivity
 import com.example.bupazar.page.fragment.ProfilePageFragment
+import com.example.bupazar.page.fragment.register.PreviousOrdersFragment
 import kotlinx.android.synthetic.main.activity_vendor_homepage.*
+import kotlinx.android.synthetic.main.activity_vendor_homepage.accountPageUserEmail
 import kotlinx.android.synthetic.main.activity_vendor_homepage.accountPageUserNameTextView
 import kotlinx.android.synthetic.main.activity_vendor_homepage.logoutLayout
 import kotlinx.android.synthetic.main.activity_vendor_homepage.messagesLayout
+import kotlinx.android.synthetic.main.activity_vendor_homepage.ordersLayout
 import kotlinx.android.synthetic.main.activity_vendor_homepage.personalInfoLayout
+import kotlinx.android.synthetic.main.fragment_my_account.*
 
 class VendorHomepageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,6 +41,17 @@ class VendorHomepageActivity : AppCompatActivity() {
             activity_vendor_2.visibility = View.GONE
             supportFragmentManager.beginTransaction().apply {
                 replace(R.id.activity_vendor_1,  profilePage)
+                commit()
+            }
+        }
+
+        ordersLayout.setOnClickListener() {
+            val previousOrdersFragment = PreviousOrdersFragment()
+            val bundle = Bundle()
+            bundle.putSerializable("USERDATA",userData)
+            previousOrdersFragment.arguments = bundle
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper,  previousOrdersFragment)
                 commit()
             }
         }
