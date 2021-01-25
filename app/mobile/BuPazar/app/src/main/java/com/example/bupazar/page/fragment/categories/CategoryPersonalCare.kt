f9eb64f3a0269010eb4aec1489dc2af2ec175ceb
Sertay Akpınar <sertayakpinar@gmail.com>
2021-01-25 04:29:41 +0300

    Categories are implemented. Enhancements on the design of the payment processes pages are implemented..

--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/fragment/categories/CategoryPersonalCare.kt
+++ app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/fragment/categories/CategoryPersonalCare.kt
@@ -1,39 +1,23 @@
 package com.example.bupazar.page.fragment.categories
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import com.example.bupazar.R
-import com.example.bupazar.page.activity.home.HomepageActivity
-import kotlinx.android.synthetic.main.fragment_home_category.*
+import com.example.bupazar.model.CategoryRequest
+import com.example.bupazar.model.SubCategoryRequest
 import kotlinx.android.synthetic.main.fragment_personal_care_category.*
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import kotlinx.android.synthetic.main.fragment_personal_care_category.buttonShowAll
+import kotlinx.android.synthetic.main.fragment_sports_category.*
 
 /**
- * A simple [Fragment] subclass.
+ * A simple CategoryPersonalCare [Fragment] subclass.
  * Use the [CategoryPersonalCare.newInstance] factory method to
  * create an instance of this fragment.
  */
 class CategoryPersonalCare : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,44 +30,76 @@ class CategoryPersonalCare : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        buttonShowAll.setOnClickListener() {
+            val categoryRequest = CategoryRequest(
+                    CategoryName = "Personal"
+            )
+            val categoryProductsFragment = CategoryProductsFragment()
+            categoryProductsFragment.categoryRequest = categoryRequest
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper,  categoryProductsFragment)
+                commit()
+            }
+        }
+
         perfume.setOnClickListener() {
-            Toast.makeText(this.activity,"This feature will be implemented soon.", Toast.LENGTH_SHORT).show()
+            val subCategoryRequest = SubCategoryRequest(
+                    SubCategoryName = "Perfume"
+            )
+            val subCategoryProductsFragment = SubCategoryProductsFragment()
+            subCategoryProductsFragment.subCategoryRequest = subCategoryRequest
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper,  subCategoryProductsFragment)
+                commit()
+            }
         }
 
         makeup.setOnClickListener() {
-            Toast.makeText(this.activity,"This feature will be implemented soon.", Toast.LENGTH_SHORT).show()
+            val subCategoryRequest = SubCategoryRequest(
+                    SubCategoryName = "Makeup"
+            )
+            val subCategoryProductsFragment = SubCategoryProductsFragment()
+            subCategoryProductsFragment.subCategoryRequest = subCategoryRequest
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper,  subCategoryProductsFragment)
+                commit()
+            }
         }
 
         skinCare.setOnClickListener() {
-            Toast.makeText(this.activity,"This feature will be implemented soon.", Toast.LENGTH_SHORT).show()
+            val subCategoryRequest = SubCategoryRequest(
+                    SubCategoryName = "SkinCare"
+            )
+            val subCategoryProductsFragment = SubCategoryProductsFragment()
+            subCategoryProductsFragment.subCategoryRequest = subCategoryRequest
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper,  subCategoryProductsFragment)
+                commit()
+            }
         }
 
         oralCare.setOnClickListener() {
-            Toast.makeText(this.activity,"This feature will be implemented soon.", Toast.LENGTH_SHORT).show()
+            val subCategoryRequest = SubCategoryRequest(
+                    SubCategoryName = "OralCare"
+            )
+            val subCategoryProductsFragment = SubCategoryProductsFragment()
+            subCategoryProductsFragment.subCategoryRequest = subCategoryRequest
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper,  subCategoryProductsFragment)
+                commit()
+            }
         }
 
         hairCare.setOnClickListener() {
-            Toast.makeText(this.activity,"This feature will be implemented soon.", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CategoryPersonalCare.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CategoryPersonalCare().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+            val subCategoryRequest = SubCategoryRequest(
+                    SubCategoryName = "HairCare"
+            )
+            val subCategoryProductsFragment = SubCategoryProductsFragment()
+            subCategoryProductsFragment.subCategoryRequest = subCategoryRequest
+            requireActivity().supportFragmentManager.beginTransaction().apply {
+                replace(R.id.fl_wrapper,  subCategoryProductsFragment)
+                commit()
             }
+        }
     }
 }
