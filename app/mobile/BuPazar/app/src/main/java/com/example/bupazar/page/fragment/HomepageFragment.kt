ed7fe1bc9be31e49f684df4048731a0687f46423
Sertay Akpınar <sertayakpinar@gmail.com>
2021-01-26 13:10:59 +0300

    Toast is imported.

--- app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/fragment/HomepageFragment.kt
+++ app/mobile/BuPazar/app/src/main/java/com/example/bupazar/page/fragment/HomepageFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.bupazar.R
